d1765c2438e248625648c6dabe7f8a802e7ea28b
minthantkyaw28 <wanderhobby@gmail.com>
2024-01-30 11:15:59 +0630

    Next App finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
